1e2960d4df489358e3225f9e021b18e6c1aeeb38
Matej Samaržija <matej.samarzija@fer.hr>
2025-10-25 14:08:06 +0200

    Login UI

--- izvorni_kod/frontend/src/App.tsx
+++ izvorni_kod/frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import './css/App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import { Routes, Route } from 'react-router-dom'
+
 function App() {
 
   return (
     <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Home />} /> // Placeholder for Login page - trenutno sam stavio Home
+        <Route path='/login' element={<Login />} />
       </Routes>
     </main>
   );
