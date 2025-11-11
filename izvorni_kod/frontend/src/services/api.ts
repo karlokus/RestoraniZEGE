655a42f4f7aa1aade6eb0380fd7e71fbd7df5093
igabrilo <ivan.gabrilo@fer.hr>
2025-11-11 17:01:42 +0100

    front-deploy innit

--- izvorni_kod/frontend/src/services/api.ts
+++ izvorni_kod/frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (import.meta.env.PROD ? 'https://restoranizege.onrender.com' : 'http://localhost:3000');
 
 export interface RegisterData {
    firstName: string;
